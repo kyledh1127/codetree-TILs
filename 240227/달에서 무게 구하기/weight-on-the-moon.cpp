427662dcb29c6ebc27ee0ce2e90778b96e3c24ca
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-02-27 20:27:46 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=13;
+    double b=0.165000;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
