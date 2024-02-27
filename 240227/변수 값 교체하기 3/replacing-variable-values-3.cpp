839d63cb9e9a776c89a6ca052ed506d944f49c84
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-02-27 21:18:46 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=3, b=5;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<endl<<b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
