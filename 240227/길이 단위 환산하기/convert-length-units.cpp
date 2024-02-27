1aa9f09eb85f22ade6d3f3a2c2fc4c3cc3477e98
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-02-27 21:49:22 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double n;
+    cin>>n;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<30.48*n;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
