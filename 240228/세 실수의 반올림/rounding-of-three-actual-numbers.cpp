4a8dcab5672a16befdbd9f4cf81503d9fbb533be
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-02-28 23:12:09 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b,c;
+    cout<<fixed;
+    cin>>a>>b>>c;
+    cout.precision(3);
+    cout<<a<<endl<<b<<endl<<c;
+    return 0;
+}
