78928c99fa0c5ba2a871c42f6f1a44e2fee745d1
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-02-28 23:08:28 +0900

    Title: 실수 입력받아 계산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b;
+    cout<<fixed;
+    cin>>a>>b;
+    cout.precision(2);
+    cout<<a+b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
