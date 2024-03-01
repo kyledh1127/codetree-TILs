932f040f687ca4d04620686287de51f27dd40319
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-01 17:03:16 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    char m,w;
+    cin>>a>>m;
+    cin>>b>>w;
+    cout<<((a>=19&&b>=19)||m=='M'||w=='M');
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
