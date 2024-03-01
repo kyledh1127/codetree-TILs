646836be5a7bc0bdd82a5b53bb7aae9eb368a58e
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-01 15:37:42 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a==1){
+        cout<<"John";
+    }
+    else if(a==2){
+        cout<<"Tom";
+    }
+    else if(a==3){
+        cout<<"Paul";
+    }
+    else{
+        cout<<"Vacanxy";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
