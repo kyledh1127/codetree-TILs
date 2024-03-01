5107160b287f75e1bec913c864dd2be0951bd357
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-01 17:16:49 +0900

    Title: 최댓값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>=b&&c>=b){
+        if(a>=c){
+            cout<<a;
+        }
+        else{
+            cout<<c;
+        }
+    }
+    if(b>=a&&c>=a){
+        if(b>=c){
+            cout<<b;
+        }
+        else{
+            cout<<c;
+        }
+    }
+    else{
+        cout<<c;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
