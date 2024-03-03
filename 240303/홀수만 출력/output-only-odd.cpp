ea51556fd361613e0cf942f1f1906e2b4dcebd06
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-03 20:50:01 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<=b;i+=2){
+        if(a%2==0){
+            cout<<i+1<<" ";
+        }
+        else{
+            cout<<i<<" ";
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
