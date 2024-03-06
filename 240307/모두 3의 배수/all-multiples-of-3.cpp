eaf6718d801b9b0f968c814f03c54ed486089b8f
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-07 01:30:19 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/모두 3의 배수/all-multiples-of-3.cpp
+++ 240307/모두 3의 배수/all-multiples-of-3.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int n,count=0;
     bool sa=true;
     for(int i=0;i<5;i++){
         cin>>n;
         if(n%3!=0){
-            sa=false;
-            cout<<0;
+            count++;
         }
 
     }
-    if(sa=true){
+    if(count==0){
         cout<<1;
+    }
+    else{
+        cout<<0;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
