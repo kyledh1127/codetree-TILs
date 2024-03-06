51df7fe537dda07458f481a45ba90efb4cba47f9
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-07 00:20:14 +0900

    Title: 합성수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    bool satisfied= false;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            satisfied = true;
+        }
+    }
+    if(satisfied==true){
+        cout<<"C";
+    }
+    else{
+        cout << "N";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
