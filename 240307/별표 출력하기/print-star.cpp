2a01d13bb6658854c9d59b4a543cf4d0b471e77b
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-07 10:13:50 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/별표 출력하기/print-star.cpp
+++ 240307/별표 출력하기/print-star.cpp
@@ -10,7 +10,7 @@ int main() {
         cout << endl;
     }
     for( int i=1; i<n; i++){
-        for(int j=0;j<5-i;j++){
+        for(int j=0;j<n-i;j++){
             cout << "*"<< " ";
         }
         cout << endl;
