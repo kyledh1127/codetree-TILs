39b88660d237cc2344e3793563d343efd7c8a6e5
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-03-17 18:22:44 +0900

    Title: 나눗셈의 나머지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240317/나눗셈의 나머지/remainder-of-division.cpp
+++ 240317/나눗셈의 나머지/remainder-of-division.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[4],sum=0;
     int a,b;
     cin>>a>>b;
-    for(int i=0;i<4;i++){
+    int arr[b],sum=0;
+    for(int i=0;i<b;i++){
         arr[i]=0;
     }
     
     while(a>1){
-        if(a%b==0){
-            arr[0]++;
-        }
-        else if(a%b==1){
-           arr[1]++;
-        }
-        else if(a%b==2){
-            arr[2]++;
-        }
-        else if(a%b==3){
-            arr[3]++;
-        }
+        int laft;
+        laft= a%b;
+        arr[laft]++;
         a=a/b;
     }
-    sum=(arr[0]*arr[0])+(arr[1]*arr[1])+(arr[2]*arr[2])+(arr[3]*arr[3]);
+    for(int i=0; i<b;i++){
+        sum+= arr[i]*arr[i];
+    }
+    
     cout<<sum;
 
     // 여기에 코드를 작성해주세요.
