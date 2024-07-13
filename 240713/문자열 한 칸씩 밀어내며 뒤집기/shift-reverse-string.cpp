84c4b1bdcf41b92a58de0a0ae61648a03e6e62fe
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-07-13 23:07:25 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

--- 240713/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
+++ 240713/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
@@ -4,26 +4,28 @@ using namespace std;
 int main() {
     string s;
     int n;
-    cin >> s >> n;
-    for(int i = 0; i < n; i++) {
+    cin>>s>>n;
+    for(int i=0;i<n;i++){
         int num;
-        cin >> num;
-        int len = s.length();
-        if(num == 1) {
-            s = s.substr(1, len - 1) + s[0];
+        cin>>num;
+        int len=s.length();
+        if(num==1){
+            s=s.substr(1,len-1)+s.substr(0,1);
+            cout<<s<<"\n";
+        }
+        if (num == 2) {
+            s = s[len - 1] + s.substr(0, len - 1); // 문자열의 마지막 문자를 앞으로 이동
             cout << s << "\n";
         }
-        else if(num == 2) {
-            s = s[len - 1] + s.substr(0, len - 1);
-            cout << s << "\n";
-        }
-        else if(num == 3) {
-            string w="";
-            for(int j = len - 1; j >= 0; j--) {
-                w+=s[j];
+        if(num==3){
+            string reversed_s = "";
+            for (int j = len - 1; j >= 0; j--) {
+                reversed_s += s[j];
             }
-            cout << s<<"\n";
+            s = reversed_s;  // 뒤집힌 문자열로 s 업데이트
+            cout << s << "\n";
         }
     }
+    // 여기에 코드를 작성해주세요.
     return 0;
 }
