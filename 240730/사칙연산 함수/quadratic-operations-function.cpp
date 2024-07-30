e4ca8065663326d4f0f70ca3faca7fc398c4ebec
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-07-30 21:43:20 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/사칙연산 함수/quadratic-operations-function.cpp
+++ 240730/사칙연산 함수/quadratic-operations-function.cpp
@@ -16,18 +16,25 @@ int main() {
     int a,b;
     string c;
     cin>>a>>c>>b;
-    cout<<a<<" "<<c<<" "<<b<<" = ";
+   
     if(c=="+"){
+        cout<<a<<" "<<c<<" "<<b<<" = ";
         sum(a,b);
     }
     else if(c=="-"){
+         cout<<a<<" "<<c<<" "<<b<<" = ";
         del(a,b);
     }
     else if(c=="*"){
+         cout<<a<<" "<<c<<" "<<b<<" = ";
         rad(a,b);
     }
     else if(c=="/"){
+         cout<<a<<" "<<c<<" "<<b<<" = ";
         dick(a,b);
+    }
+    else{
+        cout<<"False";
     }
     // 여기에 코드를 작성해주세요.
     return 0;
