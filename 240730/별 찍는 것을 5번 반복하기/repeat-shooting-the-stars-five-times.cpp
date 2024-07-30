f1a20a1da7c61cc41fc6fa8fe328a86c83bba64e
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-07-30 20:02:41 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void pr(){
+    for(int i=0;i<10;i++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+int main() {
+    for(int i=0;i<5;i++){
+        pr();
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
