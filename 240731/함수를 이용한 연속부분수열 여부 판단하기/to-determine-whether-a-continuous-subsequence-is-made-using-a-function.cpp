33c7a9b6f84592557e180a19dc8978923d0e0e3c
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-07-31 21:13:29 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240731/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 using namespace std;
-void find(int a,int b){
-    int* arr= new int[a];
-    int* brr=new int[b];
-    for(int i=0;i<a;i++){
-        cin>>arr[i];
+
+void find(int a, int b) {
+    int* arr = new int[a];
+    int* brr = new int[b];
+    
+    for (int i = 0; i < a; i++) {
+        cin >> arr[i];
     }
-    for(int i=0;i<b;i++){
-        cin>>brr[i];
+    for (int i = 0; i < b; i++) {
+        cin >> brr[i];
     }
-    int count=0;
-    int k=1;
-    for(int i=0;i<a;i++){
-        if(arr[i]==brr[0]){
-            k=0;
-            for(int j=0;j<b;j++){
-                if(arr[i+j]==brr[j]){
-                    count++;
-                }
-            }
-            if(count==b){
-                cout<<"Yes";
+    
+    bool found = false;
+    for (int i = 0; i <= a - b; i++) {  // 첫 배열의 범위 내에서만 검사
+        bool match = true;
+        for (int j = 0; j < b; j++) {
+            if (arr[i + j] != brr[j]) {
+                match = false;
                 break;
             }
-            count=0;
+        }
+        if (match) {
+            found = true;
+            break;
         }
     }
-    if(k==1){
-        cout<<"No";
+    
+    if (found) {
+        cout << "Yes";
+    } else {
+        cout << "No";
     }
+    
+    delete[] arr; // 동적 할당한 메모리 해제
+    delete[] brr;
 }
+
 int main() {
-    int a,b;
-    cin>>a>>b;
-    find(a,b);
+    int a, b;
+    cin >> a >> b;
+    find(a, b);
     return 0;
 }
