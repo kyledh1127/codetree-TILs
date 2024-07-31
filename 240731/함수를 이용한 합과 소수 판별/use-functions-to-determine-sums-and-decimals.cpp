fecfd32a062480f89354f8003394ef2310dd3752
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-07-31 20:50:26 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
+++ 240731/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
-bool find(int a){
+int find(int a){
+    int count=0;
     for(int i=2;i<a;i++){
         if(a%i==0){
-            return false;
-        }
-        else{
-            return true;
+            count++;
         }
     }
+    return count;
 }
 void ans(int a,int b){
     int c=a;
     int count=0;
     while(c<=b){
-        if(find(c)==true){
+        if(find(c)==0){
             int k= c/10;
             int j=c%10;
             if((k+j)%2==0){
@@ -23,7 +22,7 @@ void ans(int a,int b){
         }
         c++;
     }
-    cout<<count-1;
+    cout<<count;
 }
 int main() {
     // 여기에 코드를 작성해주세요.
