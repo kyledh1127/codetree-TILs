5fc634e5bbcc214419e5e7f487ef423cdaf57d31
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-08-04 18:56:52 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240804/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 240804/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 void find(string a){
     int count=0;
-    
-    for(int i=0;i<a.length();i++){
-        if(a[i]==a[i+1]){
-            a[i]=a[i+1];
+    int k=a[0];
+    for(int i=0;i<a.length()-1;i++){
+        if(k==a[i+1]){
+            k=a[i+1];
         }
         else{
             count++;
+            break;
         }
 
     }
