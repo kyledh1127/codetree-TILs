cbae7056a8902c6d684547766588c2027280eca5
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-08-11 21:02:47 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240811/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 240811/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include<vector>
 using namespace std;
 
-int main() {
+int main(){
     int num;
     cin>>num;
-    string* arr=new string[num];
-    string* brr=new string[num];
-    for(int i=0;i<num;i++){
+    string arr;
+    string brr;
+    for(int i=0;i<arr.length();i++){
         cin>>arr[i];
     }
-    for(int i=0;i<num;i++){
+    for(int i=0;i<brr.length();i++){
         cin>>brr[i];
     }
-    sort(arr,arr+num);
-    sort(brr,brr+num);
+    sort(arr.begin(), arr.end());
+    sort(brr.begin(), brr.end());
     int ans=0;
-    for(int i=0;i<num;i++){
+    if(arr.length()>brr.length()){
+        for(int i=0;i<brr.length();i++){
         if(arr[i]!=brr[i]){
             ans++;
         }
+        }
     }
+    else{
+        for(int i=0;i<arr.length();i++){
+            if(arr[i]!=brr[i]){
+                ans++;
+            }
+        }
+    }
+    
     if(ans==0){
         cout<<"Yes";
     }
