0679bf77e4b5c6c3848d12c27f9965ef73caa3dc
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-08-12 21:06:40 +0900

    Title: 중앙값 계산 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/중앙값 계산 2/get-median-2.cpp
+++ 240812/중앙값 계산 2/get-median-2.cpp
@@ -8,13 +8,12 @@ int main() {
     int* arr= new int[num];
     for(int i=0;i<num;i++){
         cin>>arr[i];
-    }
-    sort(arr,arr+num);
-    for(int i=1;i<=num;i++){
-        if(i%2==1){
-           
-            cout<<arr[i/2]<<" ";
+        sort(arr,arr+i+1);
+        if((i+1)%2==1){
+             cout<<arr[i/2]<<" ";
         }
     }
+    
+   
     return 0;
 }
