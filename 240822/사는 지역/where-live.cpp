60c1a502ddbafeeca61594a48dce58c9e4339065
kyledh1127 <80040644+kyledh1127@users.noreply.github.com>
2024-08-22 21:48:41 +0900

    Title: 사는 지역, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240822/사는 지역/where-live.cpp
+++ 240822/사는 지역/where-live.cpp
@@ -4,7 +4,7 @@ class per{
     public:
     string name,phn,city;
     int a;
-    per(string name,string phn,string city,int a){
+    per(string name,string phn,string city){
         this->name=name;
         this->phn=phn;
         this->city=city;
@@ -16,23 +16,23 @@ int main() {
     int a;
     int num;
     cin>>num;
-    per per1(name,phn,city,1);
-    per per2(name,phn,city,2);
-    per per3(name,phn,city,3);
+    per per1(name,phn,city);
+    per per2(name,phn,city);
+    per per3(name,phn,city);
     cin>>per1.name>>per1.phn>>per1.city;
     cin>>per2.name>>per2.phn>>per2.city;
     cin>>per3.name>>per3.phn>>per3.city;
-    if(per1.a==num){
+    if(per1.name>per2.name&&per1.name>per2.name){
         cout<<"name "<<per1.name<<"\n";
         cout<<"addr "<<per1.phn<<"\n";
         cout<<"city "<<per1.city<<"\n";
     }
-    if(per2.a==num){
+    else if(per2.name>per1.name&&per2.name>per3.name){
         cout<<"name "<<per2.name<<"\n";
         cout<<"addr "<<per2.phn<<"\n";
         cout<<"city "<<per2.city<<"\n";
     }
-    if(per3.a==num){
+    else if(per3.name>per1.name&&per3.name>per2.name){
         cout<<"name "<<per3.name<<"\n";
         cout<<"addr "<<per3.phn<<"\n";
         cout<<"city "<<per3.city<<"\n";
